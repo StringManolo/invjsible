d137c5bc6bb2cb84f2a4a87f2de04c4cdcea38f6
StringManolo <manuelvarelacaldas@gmail.com>
2025-12-16 12:04:34 +0000

    Chore: manual version update

--- invjsible.js
+++ invjsible.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-       const path = require('path');
+const path = require('path');
 const zlib = require('zlib');
 const { promisify } = require('util');
 
